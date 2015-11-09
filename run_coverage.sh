57dec8fa23dbfe04a9b4d84ef74f450e3d57e6a9
Joseph Kahn <JosephBKahn@gmail.com>
2015-11-09 11:36:07 -0500

    retox

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-coverage run --source=django_sharding,django_sharding_library ./runtests.py
 coverage report -m
 coverage html -d $CIRCLE_ARTIFACTS
 [[ ! -z "$$COVERALLS_REPO_TOKEN" ]] && coveralls || true;

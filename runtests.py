c739465486854d1e59ece450054fd935f74daaa7
Kosei Kitahara <surgo.jp@gmail.com>
2016-04-27 21:20:52 +0900

    Switched to the travis-ci

--- runtests.py
+++ runtests.py
@@ -6,38 +6,38 @@ try:
     from django.test.utils import get_runner
     from django_sharding_library.settings_helpers import database_configs
 
-    CIRCLECI = os.getenv("CIRCLECI", False)
+    TRAVISCI = os.getenv("TRAVIS", False)
 
     DATABASES = database_configs(databases_dict={
         'unsharded_databases': [
             {
                 'name': 'default',
                 'environment_variable': 'DATABASE_URL',
-                'default_database_url': 'postgres://sharding:sharding@localhost/sharding' if CIRCLECI else 'sqlite://testing123'
+                'default_database_url': 'postgres://postgres:@localhost/default' if TRAVISCI else 'sqlite://testing123'
             }
         ],
         'sharded_databases': [
             {
                 'name': 'app_shard_001',
                 'environment_variable': 'SHARD_001_DATABASE_URL',
-                'default_database_url': 'postgres://sharding:sharding@localhost/sharding_001' if CIRCLECI else 'sqlite://testing124',
+                'default_database_url': 'postgres://postgres:@localhost/sharding_001' if TRAVISCI else 'sqlite://testing124',
                 'replicas': [
                     {
                         'name': 'app_shard_001_replica_001',
                         'environment_variable': 'REPLICA_001_DATABASE_URL',
-                        'default_database_url': 'postgres://sharding:sharding@localhost/sharding_replica_001' if CIRCLECI else 'sqlite://testing125'
+                        'default_database_url': 'postgres://postgres:@localhost/sharding_replica_001' if TRAVISCI else 'sqlite://testing125'
                     },
                     {
                         'name': 'app_shard_001_replica_002',
                         'environment_variable': 'REPLICA_002_DATABASE_URL',
-                        'default_database_url': 'postgres://sharding:sharding@localhost/sharding_replica_002' if CIRCLECI else 'sqlite://testing126'
+                        'default_database_url': 'postgres://postgres:@localhost/sharding_replica_002' if TRAVISCI else 'sqlite://testing126'
                     },
                 ]
             },
             {
                 'name': 'app_shard_002',
                 'environment_variable': 'SHARD_002_DATABASE_URL',
-                'default_database_url': 'mysql://sharding:sharding@localhost/sharding_002' if CIRCLECI else 'sqlite://testing127'
+                'default_database_url': 'mysql://travis:@localhost/sharding_002' if TRAVISCI else 'sqlite://testing127'
             },
         ]
     })

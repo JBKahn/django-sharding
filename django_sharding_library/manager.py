39a450aa7283fed885337f87e43403db57a48b9d
Titus Peterson <titus@shopventory.com>
2016-05-01 14:01:32 -0700

    Fixed a bug with staticmethod not working properly in the manager

--- django_sharding_library/manager.py
+++ django_sharding_library/manager.py
@@ -58,19 +58,17 @@ class ShardManager(Manager):
 
         return ShardQuerySet(model=self.model)
 
-    @staticmethod
-    def _wrap(func_name):
-        def wrapped(self, **kwargs):
-            # Should check for good kwarg here as well, need to grab it off the model but not sure where/how to define
-            # that. Will ask author later
-
-            # self.get_query_set(key=int(key))
-            return getattr(self.get_query_set(), func_name)(**kwargs)
-
-        wrapped.__name__ = func_name
-        return wrapped
-
     filter = _wrap('filter')
     get = _wrap('get')
     create = _wrap('create')
     get_or_create = _wrap('get_or_create')
+
+
+def _wrap(func_name):
+    def wrapped(self, **kwargs):
+        # Should check for good kwarg here, need to grab it off the model
+        # self.get_query_set(key=int(key))
+        return getattr(self.get_query_set(), func_name)(**kwargs)
+
+    wrapped.__name__ = func_name
+    return wrapped

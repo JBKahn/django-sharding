62b15dbcb29bf1f7ce7cf8d34325dffc64a5dadd
Titus Peterson <titus@shopventory.com>
2016-05-23 13:12:48 -0700

    Removed delete()'s from tests, travis did not error with the test db settings like circle'

--- tests/test_router.py
+++ tests/test_router.py
@@ -66,8 +66,6 @@ class RouterReadTestCase(TestCase):
             self.assertIn(lookups_to_find, read_route_function.call_args)
 
     def test_queryset_router_filter_returns_existing_objects(self):
-        # Cant for sure know what order tests ran in, lets make sure the delete function works in this test
-        # TestModel.objects.filter(user_pk=self.user.pk).delete()
         for i in range(1, 11):
             test_model_obj = TestModel.objects.create(user_pk=self.user.pk, random_string="%s" % i)
             self.assertIn(test_model_obj._state.db, ['app_shard_001', 'app_shard_002'])
@@ -84,8 +82,6 @@ class RouterReadTestCase(TestCase):
         self.assertEqual(len(test_models), 20)
 
     def test_queryset_router_filter_with_aggregates(self):
-        # Cant for sure know what order tests ran in, lets make sure the delete function works in this test
-        # TestModel.objects.filter(user_pk=self.user.pk).delete()
         for i in range(1, 11):
             TestModel.objects.create(user_pk=self.user.pk, random_string="%s" % i)
         num_models = TestModel.objects.filter(user_pk=self.user.pk).count()

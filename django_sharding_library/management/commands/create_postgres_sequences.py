95297f37eec5f2ab6d118424324f98e6a2623fe0
Joseph Kahn <jkahn@waveaccounting.com>
2016-09-03 17:19:13 -0400

    handle no style

--- django_sharding_library/management/commands/create_postgres_sequences.py
+++ django_sharding_library/management/commands/create_postgres_sequences.py
@@ -31,10 +31,10 @@ class Command(BaseCommand):
         )
 
     def get_all_but_replica_dbs(self):
-        return list(filter(
+        return sorted(list(filter(
             lambda db: not settings.DATABASES[db].get('PRIMARY', None),
             settings.DATABASES.keys()
-        ))
+        )))
 
     def handle(self, *args, **options):
         if not options['database'] or options['database'] == 'all':
@@ -57,9 +57,9 @@ class Command(BaseCommand):
                     create_postgres_shard_id_function(sequence_name=sequence_name, db_alias=database, shard_id=shard_id)
                 if not verify_postres_id_field_setup_correctly(sequence_name=sequence_name, db_alias=database, function_name="next_sharded_id"):
                     raise Exception("The sequence could not be found.")
-                self.stdout.write(getattr(self.style, "SUCCESS")("\nDatabase {} with shard id {}:sequence {} is present").format(database, shard_id, sequence_name))
+                self.stdout.write(getattr(self.style, "SUCCESS", lambda a: a)("\nDatabase {} with shard id {}:sequence {} is present").format(database, shard_id, sequence_name))
             except Exception as e:
-                self.stdout.write(getattr(self.style, "ERROR")("\nDatabase {}: Error occured: {}").format(database, str(e)))
+                self.stdout.write(getattr(self.style, "ERROR", lambda a: a)("\nDatabase {}: Error occured: {}").format(database, str(e)))
                 has_errors = True
 
         if has_errors:

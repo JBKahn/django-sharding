be6a725983c7b4f532b243a4fe9668a0e1fa5418
Joseph Kahn <jkahn@waveaccounting.com>
2016-09-03 17:13:58 -0400

    upgrade tox while I am at it

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=['Django>=1.8', 'dj-database-url==0.4.1'],
-    tests_require=['psycopg2==2.6.2', 'mysqlclient==1.3.7', 'mock==2.0.0', 'django_nose==1.4.4', 'tox==2.1.1'],
+    tests_require=['psycopg2==2.6.2', 'mysqlclient==1.3.7', 'mock==2.0.0', 'django_nose==1.4.4', 'tox==2.3.1'],
     license="BSD",
     zip_safe=False,
     keywords='django shard sharding library',

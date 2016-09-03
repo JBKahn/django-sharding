0643ddc47b1faf738a5d4aa410b88345a6dd2300
Joseph Kahn <jkahn@waveaccounting.com>
2016-09-03 16:41:53 -0400

    fix path while I write tests

--- tests/test_create_postgres_sequences.py
+++ tests/test_create_postgres_sequences.py
@@ -3,9 +3,9 @@ from django.test import TestCase
 from mock import patch
 
 
-@patch('django_sharding_library.management.command.create_postgres_sequences.create_postgres_global_sequence')
-@patch('django_sharding_library.management.command.create_postgres_sequences.create_postgres_shard_id_function')
-@patch('django_sharding_library.management.command.create_postgres_sequences.verify_postres_id_field_setup_correctly')
+@patch('django_sharding_library.management.commands.create_postgres_sequences.create_postgres_global_sequence')
+@patch('django_sharding_library.management.commands.create_postgres_sequences.create_postgres_shard_id_function')
+@patch('django_sharding_library.management.commands.create_postgres_sequences.verify_postres_id_field_setup_correctly')
 class MigrationCommandTestCase(TestCase):
 
     def test_defauls_migrates_all_primary_dbs(self, mock_verify, mock_create_func, mock_create_sequence):

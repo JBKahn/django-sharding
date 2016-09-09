ab9ce7e4735c4def2bef1504bf18042e53372562
Titus Peterson <titus@shopventory.com>
2016-09-08 18:06:55 -0700

    Fixed a typo in the db settings for tests

--- runtests.py
+++ runtests.py
@@ -51,13 +51,13 @@ DATABASES = database_configs(databases_dict={
             'name': 'app_shard_003',
             'shard_group': 'postgres',
             'environment_variable': 'SHARD_003_DATABASE_URL',
-            'default_database_url': 'postgres://postgres:@localhost/sharding_003' if TRAVISCI else 'sqlite://testing128'
+            'default_database_url': 'postgres://postgres:@localhost/shard_003' if TRAVISCI else 'sqlite://testing128'
         },
         {
             'name': 'app_shard_004',
             'shard_group': 'postgres',
             'environment_variable': 'SHARD_004_DATABASE_URL',
-            'default_database_url': 'postgres://postgres:@localhost/sharding_004' if TRAVISCI else 'sqlite://testing129'
+            'default_database_url': 'postgres://postgres:@localhost/shard_004' if TRAVISCI else 'sqlite://testing129'
         },
     ]
 })

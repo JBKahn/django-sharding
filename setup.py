d8d5bf7af5aaec8e6a051e155f692cff33ec3001
Joseph Kahn <jkahn@waveaccounting.com>
2017-09-10 14:15:17 -0400

    WIP

--- setup.py
+++ setup.py
@@ -29,7 +29,8 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
     ],
     test_suite='runtests.run_tests',
 )
